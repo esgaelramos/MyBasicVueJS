4c2f848714b629871e62fc5a3db2a9f9b2413b91
Gael Ramos <esgaelramos@gmail.com>
2021-12-05 20:48:46 -0600

    Create first excersives

---
+++ instancias.js
@@ -0,0 +1,17 @@
+var instanciaUnoSexy = new Vue({
+    el: '#instanciaUno',
+    data: {
+        mensaje: 'Hola desde la instancia uno',
+        numero1: 10,
+        numero2: 20,
+    }
+})
+
+var instanciaDosSexy = new Vue({
+    el: '#instanciaDos',
+    data: {
+        mensaje: 'Hola desde la instancia que prueba el mismo nombre pero con distintas instancias',
+        mensaje2: 'Hola desde la instancia dos',
+
+    }
+})
